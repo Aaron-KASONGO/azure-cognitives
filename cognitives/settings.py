8f8bd6f53be1228ab0187dcbaeab01998cc023ef
Aaron KASONGO <19kk181@esisalama.org>
2022-01-24 15:37:26 +0100

    Initial deployment

--- cognitives/settings.py
+++ cognitives/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,16 +32,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'computer_vision.apps.ComputerVisionConfig'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'cognitives.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ computer_vision/img_vision.py
@@ -0,0 +1,71 @@
+from dotenv import load_dotenv
+import os
+from array import array
+from PIL import Image, ImageDraw
+import sys
+import time
+from matplotlib import pyplot as plt
+import numpy as np
+
+# Import namespaces
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+
+def main():
+    global cv_client
+
+    try:
+        # Get Configuration Settings
+        load_dotenv()
+        cog_endpoint = os.getenv('COG_SERVICE_ENDPOINT')
+        cog_key = os.getenv('COG_SERVICE_KEY')
+
+        # Get image
+        image_file = 'images/street.jpg'
+        if len(sys.argv) > 1:
+            image_file = sys.argv[1]
+
+        # Authenticate Computer Vision client
+        credential = CognitiveServicesCredentials(cog_key) 
+        cv_client = ComputerVisionClient(cog_endpoint, credential)
+
+        # Analyze image
+        AnalyzeImage(image_file)
+
+        # Generate thumbnail
+        GetThumbnail(image_file)
+
+    except Exception as ex:
+        print(ex)
+
+def AnalyzeImage(image_file):
+    print('Analyzing', image_file)
+
+    # Specify features to be retrieved
+    features = [VisualFeatureTypes.description,
+                VisualFeatureTypes.tags,
+                VisualFeatureTypes.categories,
+                VisualFeatureTypes.brands,
+                VisualFeatureTypes.objects,
+                VisualFeatureTypes.adult]
+    
+    
+    # Get image analysis
+    with open(image_file, mode="rb") as image_data:
+        analysis = cv_client.analyze_image_in_stream(image_data , features)
+
+    # Get image description
+    for caption in analysis.description.captions:
+        print("Description: '{}' (confidence: {:.2f}%)".format(caption.text, caption.confidence * 100))
+
+
+def GetThumbnail(image_file):
+    print('Generating thumbnail')
+
+    # Generate a thumbnail
+
+
+if __name__ == "__main__":
+    main()
