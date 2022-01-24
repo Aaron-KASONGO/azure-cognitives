dd707db08b8d1dbcc0804919e31c159b8ef861dc
Aaron KASONGO <19kk181@esisalama.org>
2022-01-24 15:53:32 +0100

    Configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cognitives.wsgi
---
+++ cognitives/prod_settings.py
@@ -0,0 +1,17 @@
+from .settings import *
+import dj_database_url
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+##INSTALLED_APPS += ['whitenoise.runserver_nostatic']
+
+WHITENOISE_USE_FINDERS = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DATABASES['default'] = dj_database_url.config()
+
+ALLOWED_HOSTS = ['imagedescription.herokuapp.com']
